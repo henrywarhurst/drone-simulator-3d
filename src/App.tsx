894244c3771124fa6e67bb20dedfe02dcd06bca3
Henry Warhurst <warhursthenry@gmail.com>
2024-05-01 19:48:02 +0200

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { OrbitControls, Plane } from '@react-three/drei';
-import * as THREE from 'three';
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Plane } from "@react-three/drei";
+import * as THREE from "three";
 
-function Scene() {
-  // Assuming the map tile is located at 'path/to/your/map-tile.jpg'
-  const texture = useLoader(THREE.TextureLoader, 'path/to/your/map-tile.jpg');
+export function App() {
+    const texture = useLoader(THREE.TextureLoader, "logo512.png");
 
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <Plane args={[10, 10]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-        <meshStandardMaterial attach="material" map={texture} />
-      </Plane>
-      <OrbitControls />
-    </Canvas>
-  );
+    return (
+        <Suspense fallback={null}>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 10]} />
+                <Plane
+                    args={[10, 10]}
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    position={[0, 0, 0]}
+                >
+                    <meshStandardMaterial attach="material" map={texture} />
+                </Plane>
+
+                <OrbitControls />
+            </Canvas>
+        </Suspense>
+    );
 }
-
-export default function App() {
-  return <Scene />;
-}
-
